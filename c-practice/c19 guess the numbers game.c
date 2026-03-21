ff21db18a748436d8903bf10a8590378b51809b9
zhang shao qi <2147376322@qq.com>
2026-03-21 12:53:19 +0800

    Create c19 guess the numbers game.c

---
+++ c-practice/c19 guess the numbers game.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//int a;
+//int main()
+//{
+//	srand(time(NULL));
+//	int num = rand() % 100 + 1;     //Generate a random number between 1-100
+//	
+//	while (a != num) 
+//	{
+//		printf("please enter a number between 1-100:");
+//		scanf("%d", &a);
+//		if (a < num)
+//		{
+//			printf("the number you entered is too small.\n");
+//		}
+//		else if (a > num)
+//		{
+//			printf("you have entered a large number\n");
+//		}
+//		else
+//		{
+//			printf("congratulations,you guessed it right\n");
+//		}
+//	}
+//	
+//
+//	return 0;
+//}
