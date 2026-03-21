f6d8a79e6cee75348143e87bfc35e9ea5e9bc614
zhang shao qi <2147376322@qq.com>
2026-03-21 11:51:58 +0800

    Create c18 random number practice.c

---
+++ c-practice/c18 random number practice.c
@@ -0,0 +1,24 @@
+﻿//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+////  random number <stdlib.h>   standard library 
+////  srand() set the seeds
+////  rand()  get random number
+//
+//int main() 
+//{
+//	//1. get the seeds
+//	//seed: if the seed remains unchanged,the result wil remain unchanged
+//	//use the changing data as a seed     time
+//	srand(time(NULL));
+//	//2. get random number        the range of random number(defailt range):0-32767
+//	for (int i = 1;i <=10;i++) 
+//	{
+//		int num = rand() % 100 + 1;    //used to generate random numbers in any range      range(1-100)
+//		//3.output and print
+//		printf("%d\n", num);
+//	}
+//	
+//	return 0;
+//}
