0cd968bffa462abaab812dfa5d2dc96b5e9cc999
zhang shao qi <2147376322@qq.com>
2026-03-21 10:30:40 +0800

    add switch exercise code

---
+++ c-practice/c03 switch practice.c
@@ -0,0 +1,26 @@
+﻿//#include <stdio.h>
+//int main()
+//{
+//	int number;
+//	printf("pleae enter a number:\n");
+//	scanf_s("%d", &number);
+//	printf("%d\n", number);
+//	switch (number)
+//	{
+//	case 1:
+//		printf("air ticket inquiry");
+//		break;
+//	case 2:
+//		printf("air ticket reservation");
+//		break;
+//	case 3:
+//		printf("air ticket change");
+//		break;
+//	case 4:
+//		printf("Exit service");
+//	default:
+//		printf("Exit service");
+//		break;
+//	}
+//	return 0;
+//}
