2c8a0405b1a8300b59251c7a9a3af114b2ded386
zhang shao qi <2147376322@qq.com>
2026-03-21 10:36:41 +0800

    Create c02 switch practice.c

---
+++ c-practice/c02 switch practice.c
@@ -0,0 +1,28 @@
+#include <iostream>
+int main()
+{
+	int week = 1;
+	switch (week)
+	{
+	case 1:
+		printf("running");
+		break;
+	case 2:
+		printf("swimming");
+		break;
+	case 3:
+		printf("take it easy");
+		break;
+	case 4:
+		printf("spinning bike");
+		break;
+	case 5:
+		printf("boxing");
+		break;
+	case 6:
+		printf("mountain climbing");
+		break;
+	case 7:
+		printf("have a gool meal");
+		break;
+	}
