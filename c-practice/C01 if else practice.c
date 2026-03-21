80d169af3757cb38bb367d7b9dbc4a03340b403e
zhang shao qi <2147376322@qq.com>
2026-03-21 10:40:12 +0800

    Create C01 if else practice.c

---
+++ c-practice/C01 if else practice.c
@@ -0,0 +1,15 @@
+﻿//#include<stdio.h>
+//int main()
+//{
+//	int likeability;
+//	printf("please enter a number:");
+//	scanf("%d", &likeability);
+//	if (likeability >= 60)
+//	{
+//		printf("agree");
+//	}
+//	else {
+//		printf("i confessed someone else");
+//	}
+//	return 0;
+//}
