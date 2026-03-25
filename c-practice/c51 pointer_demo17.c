1758d7b82d9c91702b01054d8296a898d869aae8
zhang shao qi <2147376322@qq.com>
2026-03-25 12:45:12 +0800

    pointer practice

---
+++ c-practice/c51 pointer_demo17.c
@@ -0,0 +1,31 @@
+//#include<stdio.h>
+//int method2(num1, num2);
+//void method1();
+//int main() 
+//{
+//	/*
+//	    函数指针
+//	*/
+//
+//	//1.定义指针指向两个函数
+//	void (*p1)() = method1;
+//	int (*p2)(int, int) = method2;
+//
+//	//2.利用函数指针调用函数
+//	p1();
+//	int num = p2(10,20);
+//	printf("%d\n", num);
+//
+//	return 0;
+//}
+//
+//void method1() 
+//{
+//	printf("method1\n");
+//}
+//
+//int method2(num1, num2) 
+//{
+//	printf("method2\n");
+//	return num1 + num2;
+//}
