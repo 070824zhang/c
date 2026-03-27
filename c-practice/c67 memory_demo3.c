b54d0396eeb594deb77461a595b06a219b07181a
zhang shao qi <2147376322@qq.com>
2026-03-27 20:59:44 +0800

    memory_demo

---
+++ c-practice/c67 memory_demo3.c
@@ -0,0 +1,38 @@
+﻿//#include<stdio.h>
+//#include<stdlib.h>
+//int main() 
+//{
+//	//1.申请一片连续的空间存储10个int类型的整数
+//	int* p = malloc(10 * sizeof(int));
+//		int size = 10;
+//
+//		//2.给这片空间赋值
+//		for (int i = 0; i < size;i++) 
+//		{
+//			*(p + i) = (i + 1) * 10;
+//		}
+//		//10,20,30....
+//
+//		//3.遍历
+//		printf("空间中的数据为:\n");
+//		for (int i = 0; i < size; i++) 
+//		{
+//			printf("%d\n", *(p + i));
+//		}
+//		printf("\n");
+//
+//		//4.释放空间
+//		free(p);
+//
+//		//5.释放之后再次进行遍历
+//		printf("释放之后再次遍历数据为:\n");
+//		for (int i = 0; i < size; i++)
+//		{
+//			printf("%d\n", *(p + i));
+//		}
+//
+//
+//
+//
+//return 0;
+//}
---
+++ c-practice/c68 memory_demo4.c
@@ -0,0 +1,33 @@
+﻿//#include<stdio.h>
+//#include<stdlib.h>
+//int main() 
+//{
+//	//1.申请一片连续的空间存储10个int类型的整数
+//	int* p1 = malloc(10 * sizeof(int));
+//	printf("最初的内存地址为%p\n", p1);
+//	int size = 10;
+//
+//	//2.给这片空间赋值
+//	for (int i = 0; i < size;i++) 
+//	{
+//		*(p1 + i) = (i + 1) * 10;
+//	}
+//
+//	//3.修改大小
+//	int* p2 = realloc(p1, 20 * sizeof(int));
+//	printf("修改之后的内存地址为%p\n", p2);
+//	size = 20;
+//
+//	//3.遍历
+//		printf("空间中的数据为:\n");
+//		for (int i = 0; i < size; i++) 
+//		{
+//			printf("%d\n", *(p2 + i));
+//		}
+//		printf("\n");
+//
+//		//如果我想释放空间
+//		free(p2);
+//
+//return 0;
+//}
